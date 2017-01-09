b3107465bbb11fd3bfeb79d9f95b6fbf86d8c9b6
SPPH <harrigan-s@outlook.com>
2017-01-09 21:56:54 +0000

    Added IP information display

--- server/server.c
+++ server/server.c
@@ -63,19 +63,19 @@ int main(int argc, char **argv)
 
 	//get IP address.
 	struct ifaddrs *ifap, *ifa;
-    struct sockaddr_in *sa;
-    char *addr;
+    	struct sockaddr_in *sa;
+    	char *addr;
+	
+    	getifaddrs (&ifap);
+    	for (ifa = ifap; ifa; ifa = ifa->ifa_next) {
+        	if (ifa->ifa_addr->sa_family==AF_INET) {
+            		sa = (struct sockaddr_in *) ifa->ifa_addr;
+            		addr = inet_ntoa(sa->sin_addr);
+            		printf("My interface: %s\tAddress: %s\n", ifa->ifa_name, addr);   
+		}
+    	}
 
-    getifaddrs (&ifap);
-    for (ifa = ifap; ifa; ifa = ifa->ifa_next) {
-        if (ifa->ifa_addr->sa_family==AF_INET) {
-            sa = (struct sockaddr_in *) ifa->ifa_addr;
-            addr = inet_ntoa(sa->sin_addr);
-            printf("My interface: %s\tAddress: %s\n", ifa->ifa_name, addr);   
-}
-    }
-
-    freeifaddrs(ifap);	
+    	freeifaddrs(ifap);	
 
 	//connection activity
 	for (;;) {
